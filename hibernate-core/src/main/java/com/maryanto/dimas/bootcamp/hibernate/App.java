743d763162d7b7e5be2b8d323c30002a77fdc114
dimasm93 <dimas_m@icloud.com>
2021-01-02 22:40:08 +0700

    init project

---
+++ hibernate-core/src/main/java/com/maryanto/dimas/bootcamp/hibernate/App.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.bootcamp.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
