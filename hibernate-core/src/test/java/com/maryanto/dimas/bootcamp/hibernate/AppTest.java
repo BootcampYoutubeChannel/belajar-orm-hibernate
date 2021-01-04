4511e4a337c17f73acd4c679a7391aa803e6e702
dimasm93 <dimas_m@icloud.com>
2021-01-04 23:23:50 +0700

    build connection hibernate

--- hibernate-core/src/test/java/com/maryanto/dimas/bootcamp/hibernate/AppTest.java
+++ hibernate-core/src/test/java/com/maryanto/dimas/bootcamp/hibernate/AppTest.java
@@ -1,33 +1,30 @@
 package com.maryanto.dimas.bootcamp.hibernate;
 
-import junit.framework.Test;
+import com.maryanto.dimas.bootcamp.hibernate.config.HibernateConfiguration;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+@Slf4j
 public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+
+    private Session session;
+
+    @Override
+    protected void setUp() throws Exception {
+        log.info("init hibernate session");
+        this.session = HibernateConfiguration.getSession();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
+    @Test
+    public void testOpenConnection() {
+        this.session.beginTransaction();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Override
+    protected void tearDown() throws Exception {
+        log.info("destroy hibernate session!");
+        this.session.close();
     }
 }
