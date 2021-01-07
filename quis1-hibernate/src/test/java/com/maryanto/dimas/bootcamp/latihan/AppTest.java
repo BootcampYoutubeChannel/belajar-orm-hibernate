c8f1de9c2a09fee72808332a514e87699d7cb3c1
dimasm93 <dimas_m@icloud.com>
2021-01-07 23:47:17 +0700

    init project hibernate core quis 1

---
+++ quis1-hibernate/src/test/java/com/maryanto/dimas/bootcamp/latihan/AppTest.java
@@ -0,0 +1,32 @@
+package com.maryanto.dimas.bootcamp.latihan;
+
+import com.maryanto.dimas.bootcamp.latihan.config.HibernateConfiguration;
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.junit.Test;
+
+@Slf4j
+public class AppTest extends TestCase {
+
+    private Session session;
+
+    @Override
+    protected void setUp() throws Exception {
+        log.info("init hibernate session");
+        this.session = HibernateConfiguration.getSession();
+    }
+
+    @Test
+    public void testOpenConnection() {
+        this.session.beginTransaction();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        log.info("destroy hibernate session!");
+        this.session.close();
+    }
+}
+
+
