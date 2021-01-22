5a055f4880af79d505180022f197b6003d3a8b2b
dimasm93 <dimas_m@icloud.com>
2021-01-22 22:48:10 +0700

    setup config hibernate

--- quis2-hibernate/src/test/java/com/maryanto/dimas/quis/bootcomp/AppTest.java
+++ quis2-hibernate/src/test/java/com/maryanto/dimas/quis/bootcomp/AppTest.java
@@ -1,38 +1,28 @@
 package com.maryanto.dimas.quis.bootcomp;
 
-import junit.framework.Test;
+import com.maryanto.dimas.quis.bootcomp.config.HibernateConfiguration;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@Slf4j
+public class AppTest extends TestCase {
+
+    private Session session;
+
+    @Override
+    protected void setUp() throws Exception {
+        this.session = HibernateConfiguration.getSession();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testConnection() {
+        Transaction trx = this.session.beginTransaction();
+        log.info("connected!");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Override
+    protected void tearDown() throws Exception {
+        this.session.close();
     }
 }
